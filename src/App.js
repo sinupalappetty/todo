d935e510f7d29a5ac600bace506dc7a18bce04e0
Sinu P <sinupalappetty25@gmail.com>
2022-09-21 11:05:28 +0530

    todo added

---
+++ src/App.js
@@ -0,0 +1,68 @@
+
+import './App.css';
+import {useState} from 'react'
+
+function App() {
+  const [toDos,setToDos]=useState([])
+  const [toDo,setToDo] = useState('')
+  return (
+    <div className="app">
+      <div className="mainHeading">
+        <h1>ToDo List</h1>
+      </div>
+      <div className="subHeading">
+        <br />
+        <h2>Whoop, it's {Date(Date.now).slice(0,15)} 🌝 ☕ </h2>
+      </div>
+      <div className="input">
+        <input value={toDo} onChange={(e)=>setToDo(e.target.value)} type="text" placeholder="🖊️ Add item..." />
+        <i onClick={()=>{
+          if (toDo.trim()==""){
+            alert('Please Enter Something')
+            return
+          }
+          setToDos([...toDos,{id:Date.now(),text:toDo,status:false}])
+          setToDo('')
+        }
+        }className="fas fa-plus"></i>
+      </div>
+      <div className="todos">
+        {  toDos.map((obj)=>{
+          return(
+        <div className="todo">
+          <div className="left">
+            <input onChange={(e)=>{
+              console.log(e.target.value)
+              console.log(obj)
+              setToDos(toDos.filter(obj2=>{
+                if(obj2.id===obj.id){
+                  obj2.status=e.target.checked
+                }
+                return obj2
+              }))
+            }} checked={obj.status} type="checkbox" name="" id="" />
+            <p>{obj.text}</p>
+          </div>
+          <div className="right">
+            <i onClick={()=>{
+              setToDos(toDos.filter(obj2=>{
+                return(obj2.id!==obj.id)
+              }))
+            }} className="fas fa-times"></i>
+          </div>
+        </div>
+          )
+        }) }
+        {toDos.map((obj)=>{
+          if(obj.status){
+            return(<h1>{obj.text}</h1>)
+          }
+          return null
+        })}
+      </div>
+    </div>
+   
+  );
+}
+
+export default App;
